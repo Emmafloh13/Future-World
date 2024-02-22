fabd956e2f9eb9da17e7655add2d7c1f931b9948
Emmafloh13 <emmafh342@gmail.com>
2024-02-21 18:32:51 -0600

    1_8 React Server Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log('Hola')
   return (
     <main className={styles.main}>
       <h1>Hola</h1>
