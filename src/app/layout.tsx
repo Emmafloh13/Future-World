5db36c6731f27ad32f4558ababc996acc91f459f
Emmafloh13 <emmafh342@gmail.com>
2024-02-27 17:35:29 -0600

    2_7 Optimización de fuentes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
-import { Inter } from "next/font/google";
+import {Kdam_Thmor_Pro} from 'next/font/google'
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
 import 'app/sass/globals.sass'
 
-const inter = Inter({ subsets: ["latin"] });
+
+const kdam_thmor_pro = Kdam_Thmor_Pro({
+  weight: ['400'],
+  subsets: ["latin"],
+})
 
 export default function RootLayout({
   children,}: Readonly<{
@@ -11,7 +15,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={kdam_thmor_pro.className}>
 
       <Header/> 
 
