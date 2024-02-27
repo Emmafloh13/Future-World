c5855bde81405311bcea49414fd8822a79e4f332
Emmafloh13 <emmafh342@gmail.com>
2024-02-27 16:12:59 -0600

    2_5 Manejo y optimización de imagenes

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,8 +1,20 @@
+import styles from './Description.module.sass'
+import Image from 'next/image'
+
 export const Description = ()=>{
     return (
-        <section>
-            <img src="/img/imagen.png" alt="photo" />
+        <section className={styles.Description}>
+            <Image src="/img/imagen.png" 
+            alt="photo" 
+            width={300} 
+            height={300}
+            //priority={false}
+            />
+
+            <div className={styles.Description__text}>
             <h2>Aguacatito </h2>
+            <p>Criatura alienígena emergiendo de un pequeño "Aguacatito" confundido y asustado. </p>
+            </div>
         </section>
     )
 }
