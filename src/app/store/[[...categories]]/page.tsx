1e5bf1119f986e2f5f2da88fe243ea046b240504
Emmafloh13 <emmafh342@gmail.com>
2024-03-16 18:26:26 -0600

    4_6 Implementacion de navegacion por categoria

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,14 +1,20 @@
+import { ProductsWrapper } from "app/components/Store/ProductsWrapper"
+import { getProducts } from "app/services/shopify"
+
+
 interface CategoryProps{
     params: {
         categories: string[],
         searchParams: string
     }
 }
-export default function Category(props: CategoryProps){
+export default async function Category(props: CategoryProps){
    
+    const products = await getProducts()
+    
     const {categories} = props.params 
     // throw new Error('Error:BOOM!')
     return(
-        <h1>Category: {categories}</h1>
+        <ProductsWrapper products={products} />
     )
 }
